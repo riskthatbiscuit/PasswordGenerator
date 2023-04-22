e70832e7e2f241f95517457647316f014ff1b21c
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-04-22 15:06:20 -0400

    Adding README file and commentary to code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # PasswordGenerator
+
+This webpage is a password Generator. It accepts multiple prompts from the user to randomly generate a password of specific length, including a combination of required characters. 
+
+## Navigation ✈️
+All of the content is visble on the screen. The Generate Password button will launch the series of prompts for the user. 
+
+## Application
+The Generate Password button asks the user a series of prompts:
+    - The required length of the password (only accepts numerical values between 8 -128)
+    - A binary question to include lowercase letters
+    - A binary question to include uppercase letters
+    - A binary question to include numeric characters
+    - A binary question to include special characters  
+At the end of the questions, a randomly generated password is output into the main page.
+
+### Technologies used
+This website was built using HTML, Javascript and CSS.
+
+### Licences
+This website uses MIT Licence
+
+### Screenshot
+![Screenshot](assets/Screenshot.png)
+
+### Link to deployed website
+[Website Link](https://riskthatbiscuit.github.io/PasswordGenerator/)
--- assets/script.js
+++ assets/script.js
@@ -12,19 +12,22 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Entered Code below
+// Password generation function below
 function generatePassword() {
+  // Setting initial constants to be used in the creation of the password
   const lowerChar = "abcdefghijklmnopqrstuvwxyz";
   const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numericChar = '0123456789';
   const specialChar = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
   const options = ["lowercase", "uppercase", "numeric", "special"];
   const charList = [lowerChar, upperChar, numericChar, specialChar];
+  // Setting initial string for password creation
   let password = "";
-  
+  // Setting max and min length of password
   const minLength = 8;
   const maxLength = 128;
   
+  // Initial question regarding the length of password
   let passwordLength;
   while (true) {
     const promptResponse = prompt('Enter required length of password');
@@ -35,20 +38,24 @@ function generatePassword() {
     alert('password needs to be between ' + minLength + ' and ' + maxLength);
   }
   
+  // Looping through the sequence of 4 character options for inclusion in the password
   const optionsAnswers = []
   let selectedOptions = 0;
   do {
     for (let i = 0; i < options.length; i++) { 
       optionsAnswers[i]=confirm((i+1) + "/" + options.length + ". Confirm that you would like to include " + options[i] +  " characters");
+      // Adding to master list of characters from which password can be built
       if (optionsAnswers[i]) {
         selectedOptions++;
       }
     }
+    // Building check to see that at least one option had been selected
     if (selectedOptions === 0) {
     alert("At least one option must be selected!");
     }
   } while (selectedOptions === 0);
 
+  // Adding one random initial character to password for each option selected in previous step
   let charOptions = "";
   for (let i = 0; i < optionsAnswers.length; i++) {
     if (optionsAnswers[i] == true){
@@ -59,15 +66,18 @@ function generatePassword() {
     }
   }
 
+  // Calculating the number of additional random characters required
   let numRemaining  = passwordLength - password.length;
 
+  // Adding in remaining additional random letters selected from selectedOptions
   for (let i = 0; i < numRemaining; i++) {
     const ranNum = Math.floor(Math.random()*charOptions.length);
     const newChar = charOptions.slice(ranNum,ranNum+1);
     password += newChar;
   }
 
-  for (let i = 0; i < 3; i++){
+  // Shuffling the first 4 of the password into the rest so that structure completely random
+  for (let i = 0; i < 4; i++){
     var ranNum = Math.floor(Math.random()*(password.length));
     password = [password.slice(1, ranNum), password[0], password.slice(ranNum)].join('');
   }
